1b2297ac5676286f28a653a234739092f1d2b300
YuliaMakarova <juliamakarova2172@gmail.com>
2023-11-10 18:33:35 +0400

    инициализация проекта

---
+++ src/main/java/com/web/makarova/treasury/TreasuryApplication.java
@@ -0,0 +1,13 @@
+package com.web.makarova.treasury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreasuryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/makarova/treasury/TreasuryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.makarova.treasury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasuryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
